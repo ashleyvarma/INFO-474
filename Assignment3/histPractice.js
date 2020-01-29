85327173de5e388b570f72b3fe2829cc26fc94c7
Ashley Varma <ashleyvarm@gmail.com>
2020-01-29 11:59:36 -0800

    Add files via upload

--- Assignment3/histPractice.js
+++ Assignment3/histPractice.js
@@ -26,27 +26,31 @@ d3.csv("Admission_Predict.csv", function(data) {
     // X axis: scale and draw:
     var x = d3.scaleLinear()
         .domain([90, 122])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-        .range([0, width]);
+        .range([50, width-50]);
     svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
+        .attr("transform", "translate(0," + (height -50) + ")")
         .call(d3.axisBottom(x));
 
     // set the parameters for the histogram
     var histogram = d3.histogram()
-        .value(function(d) { return d.toeflScores; })   // I need to give the vector of value
+        .value(function(d) { return d; })   // I need to give the vector of value
         .domain(x.domain())  // then the domain of the graphic
         .thresholds(x.ticks(15)); // then the numbers of bins
 
 
     // And apply this function to data to get the bins
     var bins = histogram(toeflScores);
+    console.log(bins);
 
     // Y axis: scale and draw:
     var y = d3.scaleLinear()
-        .range([height, 0]);
+        .domain([0, d3.max(bins, function(d) { return d.length; })])
+        .range([height - 50, 50]);
         console.log(height);
-        y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+         
+        console.log(d3.max(bins, function(d) { return d.length; }))  // d3.hist has to be called before the Y axis obviously
         svg.append("g")
+        .attr("transform", "translate(50, 0)")
         .call(d3.axisLeft(y));
 
     // append the bar rectangles to the svg element
@@ -57,12 +61,12 @@ d3.csv("Admission_Predict.csv", function(data) {
             .attr("x", 1)
             .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
             .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-            .attr("height", function(d) { return height - y(d.length); })
+            .attr("height", function(d) { return (height-50) - y(d.length); })
             .style("fill", "black")
     
     svg.append('text') 
-        .attr('x', 200)
-        .attr('y', 490)
+        .attr('x', 175)
+        .attr('y', 350)
         .attr('fill', 'black')
         .text('TOEFL Score (bin)')
         
